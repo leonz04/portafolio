64c40f423043ca294111eebaaae632f5ca64a4b6
andresfleon <andresfleon@gmail.com>
2023-12-06 22:02:53 -0500

    V3.000002

--- js/js.js
+++ js/js.js
@@ -8,14 +8,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const close = document.getElementById('form__button_close');
     const close1 = document.getElementById('jsbutton');
-    const contact1 = document.getElementById('contact1');
+
 
     modal.addEventListener('click', (event) => {
         dialog.showModal();
     });
-    contact1.addEventListener('click', (event) => {
-      dialog.showModal();
-  });
+  
 
 
 
